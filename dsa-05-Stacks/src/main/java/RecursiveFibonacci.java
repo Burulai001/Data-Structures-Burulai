639886698e1bc63f3875c132ae3d8d923f3dba3c
Burulai001 <abdraburulai@gmail.com>
2022-10-30 09:12:36 -0700

    New updates

--- dsa-05-Stacks/src/main/java/RecursiveFibonacci.java
+++ dsa-05-Stacks/src/main/java/RecursiveFibonacci.java
@@ -2,10 +2,19 @@ public class RecursiveFibonacci {
     public static void main(String[] args) {
         System.out.println(fib(2));
         System.out.println(fib(5));
+        fun(5);
     }
     public static int fib(int n){
         if (n==0) return 0;// termination
         if (n==1) return 1;// termination
         return fib(n-1)+fib(n-2);
+    }
+
+    static void fun(int n){
+        if (n==0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
     }
 }
